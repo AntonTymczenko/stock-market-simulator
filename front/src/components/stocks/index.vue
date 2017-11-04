0c165a1c7162b9b909f50a4d8e1bc8e62f0f5690
Anton Tymczenko <tymczenko@protonmail.com>
2017-11-04 21:20:17 +0200

    general routing

--- front/src/components/stocks/index.vue
+++ front/src/components/stocks/index.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="container">
-  <router-view></router-view>
-</div>
+  <div>
+    <h1>Stocks - Stocks</h1>
+  </div>
 </template>
 
 <script>
@@ -9,6 +9,5 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Portfolio from '@/components/portfolio/'
+import Stocks from '@/components/stocks/'
 
 Vue.use(Router)
 
@@ -8,8 +10,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio
+    },
+    {
+      path: '/stocks',
+      name: 'stocks',
+      component: Stocks
     }
+
   ]
 })
